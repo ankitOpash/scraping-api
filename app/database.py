df6d4df64cbbbfb89720c410f04fda44de5a3ccb
ankitOpash <ay.opash@gmail.com>
2024-12-25 12:41:10 +0530

    update ssl option

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "prefer"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
